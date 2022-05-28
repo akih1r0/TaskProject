45d33a0d88510fa8d7e5c50e48307d796a15298e
Bohdan Zaichenko <ichim.akih1r0@gmail.com>
2022-05-28 23:56:38 +0200

    first commit basic MVVM pattern without any UI

---
+++ app/src/main/java/com/hvad/taskproject/TaskDao.kt
@@ -0,0 +1,26 @@
+package com.hvad.taskproject
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import java.nio.channels.SelectableChannel
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Insert
+    suspend fun insertAll(tasks: List<Task>)
+
+    @Update
+    suspend fun update(task:Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Query("SELECT * FROM task_table WHERE taskId = :taskId")
+    fun get(taskId: Long): LiveData<Task>
+
+    @Query("SELECT * FROM task_table ORDER BY taskId DESC")
+    fun getAll(): LiveData<List<Task>>
+}
---
+++ app/src/main/java/com/hvad/taskproject/TasksViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.hvad.taskproject
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class TasksViewModelFactory(private val dao: TaskDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TasksViewModel::class.java))
+        {
+            return  TasksViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown viewModel")
+    }
+
+
+}
