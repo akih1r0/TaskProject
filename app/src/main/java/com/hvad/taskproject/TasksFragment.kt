e6a80b2c789cf9c7ec3e61b7ccfadd529bfbf0df
Bohdan Zaichenko <ichim.akih1r0@gmail.com>
2022-05-29 01:25:22 +0200

    Basic Recycler, Adapter, With TextViews

--- app/src/main/java/com/hvad/taskproject/TasksFragment.kt
+++ app/src/main/java/com/hvad/taskproject/TasksFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.hvad.taskproject.databinding.FragmentTasksBinding
 
 
@@ -30,6 +32,15 @@ class TasksFragment : Fragment() {
         binding.viewModel=viewModel
         binding.lifecycleOwner=viewLifecycleOwner
 
+        //RecyclerView
+        val adapter= TaskItemAdapter()
+        binding.tasksList.adapter=adapter
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data=it
+            }
+        })
 
         return view
     }
--- app/src/main/java/com/hvad/taskproject/TasksViewModel.kt
+++ app/src/main/java/com/hvad/taskproject/TasksViewModel.kt
@@ -8,9 +8,11 @@ import kotlinx.coroutines.launch
 class TasksViewModel (val dao: TaskDao): ViewModel() {
     var newTaskName=""
     val tasks= dao.getAll()
-    val tasksString = Transformations.map(tasks){
-        tasks -> formatTasks(tasks)
-    }
+
+    //Makes one big string out of String list
+//    val tasksString = Transformations.map(tasks){
+//        tasks -> formatTasks(tasks)
+//    }
 
     fun addTask(){
         viewModelScope.launch {
@@ -21,16 +23,16 @@ class TasksViewModel (val dao: TaskDao): ViewModel() {
 
     }
 
-    fun formatTasks(tasks: List<Task>):String {
-        return tasks.fold(""){
-            str, item -> str + '\n'+formatTask(item)
-        }
-    }
-
-    fun formatTask(task: Task): String {
-        var str = "ID: ${task.taskId}"
-        str+='\n'+"Name: ${task.taskName}"
-        str+='\n'+ "Complete: ${task.taskDone}+\n"
-        return str
-    }
+//    fun formatTasks(tasks: List<Task>):String {
+//        return tasks.fold(""){
+//            str, item -> str + '\n'+formatTask(item)
+//        }
+//    }
+//
+//    fun formatTask(task: Task): String {
+//        var str = "ID: ${task.taskId}"
+//        str+='\n'+"Name: ${task.taskName}"
+//        str+='\n'+ "Complete: ${task.taskDone}+\n"
+//        return str
+//    }
 }
