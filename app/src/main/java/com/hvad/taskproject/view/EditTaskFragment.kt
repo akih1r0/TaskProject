e1527a3ce0f0b31461755e3a510db68729c468fa
Bohdan Zaichenko <ichim.akih1r0@gmail.com>
2022-05-30 02:42:04 +0200

    EditTaskFragment.kt (Binding, Navigation etc)
    FINISHED!!!! (almost xd, UI sucks)

--- app/src/main/java/com/hvad/taskproject/view/EditTaskFragment.kt
+++ app/src/main/java/com/hvad/taskproject/view/EditTaskFragment.kt
@@ -6,22 +6,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.hvad.taskproject.R
+import com.hvad.taskproject.databinding.FragmentEditTaskBinding
+import com.hvad.taskproject.model.TaskDatabase
+import com.hvad.taskproject.viewmodel.EditTaskViewModel
+import com.hvad.taskproject.viewmodel.EditTaskViewModelFactory
+import com.hvad.taskproject.viewmodel.TasksViewModel
+import com.hvad.taskproject.viewmodel.TasksViewModelFactory
 
 
 class EditTaskFragment : Fragment() {
+
+    private var _binding:FragmentEditTaskBinding?= null
+    private val binding get()= _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view=inflater.inflate(R.layout.fragment_edit_task, container, false)
 
-        val textView=view.findViewById<TextView>(R.id.task_id)
+
+        _binding= FragmentEditTaskBinding.inflate(inflater, container, false)
+        val view=binding.root
+
+
+
+        val application= requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
         val taskId=EditTaskFragmentArgs.fromBundle(requireArguments()).taskId
-        textView.text=taskId.toString()
+
+        val viewModelFactory= EditTaskViewModelFactory(taskId, dao)
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(EditTaskViewModel::class.java)
+        binding.viewModel=viewModel
+        binding.lifecycleOwner=viewLifecycleOwner
+
+
+        viewModel.navigateToList.observe(viewLifecycleOwner, Observer { navigate->
+            if (navigate){
+                val action= EditTaskFragmentDirections.actionEditTaskFragmentToTasksFragment()
+                this.findNavController().navigate(action)
+                viewModel.onNavigatedToList()
+            }
+        })
 
         return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 
 
